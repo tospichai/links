4bb9b4426970d4d1f15a3e5083cfe319ce0533b5
tospichai kumngern <tospichai@gmail.com>
2022-09-23 19:14:32 +0700

    youtube

--- resources/views/components/video-youtube.blade.php
+++ resources/views/components/video-youtube.blade.php
@@ -1,7 +1,12 @@
 <section class="p-4 flex flex-col gap-9" id="link">
     <section id="youtube">
-        <div class="relative pb-[56.25%] w-full rounded-xl overflow-hidden">
-            <iframe class="absolute top-0 left-0 w-full h-full border-0" width="100%" height="100%" src="https://www.youtube.com/embed/{{ $row->url }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div class="mb-3">
+            <h2 class="text-xl text-slate-500">{{ $category }}</h2>
         </div>
+        @foreach ($data->usercomponent()->orderBy('ordinal')->get() as $row)
+        <div class="relative pb-[56.25%] w-full rounded-xl overflow-hidden">
+            <iframe class="absolute top-0 left-0 w-full h-full border-0" width="100%" height="100%" src="https://www.youtube.com/embed/{{ explode("=",$row->url)[1] }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        @endforeach
     </section>
 </section>
